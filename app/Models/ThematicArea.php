78526b25d5bda9701873da6404a1c32a135dec98
Alberto Caparrós <albertojcr96@gmail.com>
2022-05-05 18:54:17 +0200

    - Añadido trait HasFactory

--- app/Models/ThematicArea.php
+++ app/Models/ThematicArea.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Str;
 
 class ThematicArea extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = [];
 
