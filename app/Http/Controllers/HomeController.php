81091ca57efbf0a79eb5e1a31c6ffbd7ad833e13
Alberto Caparrós <albertojcr96@gmail.com>
2022-05-12 18:33:41 +0200

    feature/reemplazar-auth-por-jetstream - Limpieza de archivo de rutas web.php - Limpieza de vistas - Limpieza de listeners, providers, policies y events - Vista welcome añadida

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth')->except(['index']);
-    }
-
     public function index()
     {
-        return view('home');
+        return view('welcome');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,18 @@
 <?php
-Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
 
-Route::group([
-    'prefix'=>'admin',
-    'namespace'=>'Admin',
-    'middleware'=>'auth',
-], function() {
-    Route::get('home', 'AdminController@index')->name('admin.dashboard');
+use Illuminate\Support\Facades\Route;
 
-    Route::resource('users', 'UsersController', ['as' => 'admin'], ['middleware' => 'Ver usuarios', 'Crear usuarios', 'Editar usuarios']);
-    Route::get('users/{user}/photo','UsersController@editphoto')->name('admin.users.photo');
-    Route::post('users/photo/update','UsersController@updatePhoto')->name('admin.users.photo.update');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-    Route::resource('places', 'PlacesController', ['except' => ['create'], 'as' => 'admin']);
-
-    Route::resource('photographies', 'PhotographiesController', ['except' => ['create'], 'as' => 'admin']);
-
-    Route::resource('pointsofinterest', 'PointsOfInterestController', ['except' => ['create'], 'as' => 'admin']);
-
-    Route::resource('visits', 'VisitsController', ['except' => ['create'], 'as' => 'admin']);
-
-    Route::resource('thematicareas', 'ThematicAreasController', ['except' => ['create'], 'as' => 'admin']);
-
-    Route::resource('videos', 'VideosController', ['except' => ['create'], 'as' => 'admin']);
-
-    Route::resource('videoitems', 'VideoItemsController', ['except' => ['index, create, store, show, destroy'], 'as' => 'admin']);
-
-    Route::resource('roles', 'RolesController', ['except' => 'show', 'as' => 'admin'], ['middleware' => 'Crear roles', 'Editar roles']);
-    Route::middleware('role:Administrador')
-        ->put('users/{user}/roles', 'UsersRolesController@update')
-        ->name('admin.users.roles.update');
-    Route::middleware('role:Administrador')
-        ->put('users/{user}/permissions', 'UsersPermissionsController@update')
-        ->name('admin.users.permissions.update');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-Route::get('password/email', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.email');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-
-// Password reset routes...
-Route::post('password/reset', 'Auth\ResetPasswordController@reset') ;
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
