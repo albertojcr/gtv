fd0b161b57b0130cbb0013601f5f51a899fb73c9
Alberto Caparrós <albertojcr96@gmail.com>
2022-05-12 20:02:15 +0200

    feature/reemplazar-auth-por-jetstream - Flowbite añadido

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './node_modules/flowbite/**/*.js',
     ],
 
     theme: {
@@ -16,5 +17,9 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('flowbite/plugin')
+    ],
 };
