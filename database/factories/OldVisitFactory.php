f031cbe60fe8ac9c9e28a0391dfd9972a8297c44
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-05-18 13:38:11 +0200

    Cambio en los factorys para poder hacer los test

---
+++ database/factories/OldVisitFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+namespace Database\Factories;
+
+use App\Models\PointOfInterest;
+use App\Models\Visit;
+use Faker\Generator as Faker;
+
+$factory->define(Visit::class, function (Faker $faker) {
+    return [
+        'hour' => $faker->dateTime(),
+        'deviceid' => $faker->uuid,
+        'appversion' => $faker->numberBetween(1, 10),
+        'useragent' => $faker->word,
+        'ssoo' => $faker->word,
+        'ssooversion' => $faker->numberBetween(1, 10),
+        'latitude' => $faker->latitude,
+        'longitude' => $faker->longitude,
+        'point_of_interest_id' => $faker->randomElement(PointOfInterest::all()->pluck('id')->toArray())
+    ];
+});
