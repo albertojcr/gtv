fe337de4f22ac26d5a1adf5cf0b9a612fc8c8809
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-06-13 20:38:21 +0200

    Mejora Show Visit

---
+++ app/Http/Livewire/Admin/Visit/ShowVisits.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Visit;
+
+use App\Models\Visit;
+use Livewire\Component;
+use Livewire\WithPagination;
+use function view;
+
+class ShowVisits extends Component
+{
+    use WithPagination;
+
+    protected $listeners = ['delete', 'render'];
+
+    public $detailsModal = [
+        'open' => false,
+        'id' => null,
+        'hour' => null,
+        'deviceid' => null,
+        'appversion' => null,
+        'useragent' => null,
+        'ssoo' => null,
+        'ssooversion' => null,
+        'latitude' => null,
+        'longitude' => null,
+        'point_of_interest_id' => null,
+    ];
+
+    public function show(Visit $visit)
+    {
+        $this->detailsModal['open'] = true;
+        $this->detailsModal['id'] = $visit->id;
+        $this->detailsModal['hour'] = $visit->hour;
+        $this->detailsModal['deviceid'] = $visit->deviceid;
+        $this->detailsModal['appversion'] = $visit->appversion;
+        $this->detailsModal['useragnt'] = $visit->useragent;
+        $this->detailsModal['ssoo'] = $visit->ssoo;
+        $this->detailsModal['ssooversion'] = $visit->ssooversion;
+        $this->detailsModal['latitude'] = $visit->latitude;
+        $this->detailsModal['longitude'] = $visit->longitude;
+        $this->detailsModal['point_of_interest_id'] = $visit->point_of_interest_id;
+    }
+
+    public function delete(Visit $visit)
+    {
+        $visit->delete();
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.show-visits', ['visits' => Visit::paginate(20)]);
+    }
+
+
+}
---
+++ resources/views/livewire/admin/visit/show-visits.blade.php
@@ -0,0 +1,184 @@
+<div>
+    <div class="flex items-center mb-6">
+        <h1 class="text-2xl font-semibold text-gray-700">Listado de vídeos</h1>
+
+        @hasanyrole('Administrador|Profesor')
+        <button type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ml-auto"
+                wire:click="$emitTo('admin.video.create-video', 'openCreationModal')">
+            Añadir
+        </button>
+        @endhasanyrole
+    </div>
+
+    @if(count($videos))
+        @livewire('admin.visit.edit-visit')
+
+        <x-table>
+            <x-slot name="thead">
+                <th scope="col" class="px-6 py-3">
+                    ID
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Device ID
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    SSOO
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    SSOO Version
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Punto de Interes
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Creador
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Actualizador
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Fecha creación
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <span class="sr-only">Actions</span>
+                </th>
+            </x-slot>
+
+            <x-slot name="tbody">
+                @foreach($visits as $visit)
+                    <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            <div>{{ $visit->id}}</div>
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            <div>{{ $visit->deviceid }}</div>
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            <div>{{ $visit->ssoo }}</div>
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            <div>{{ $visit->ssooversion }}</div>
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            <div >{{ $visit->point_of_interest_id }}</div>
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            @if($visit->updater)
+                                {{ \App\Models\User::find($video->updater)->name }}
+                            @endif
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            {{ $visit->created_at }}
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap flex gap-4">
+                            <span class="font-medium text-blue-600 cursor-pointer" wire:click="show('{{ $visit->id }}')">
+                                <i class="fa-solid fa-eye"></i>
+                            </span>
+                            <span class="font-medium text-yellow-400 cursor-pointer"
+                                  wire:click="$emitTo('admin.visit.edit-visit', 'openEditModal', '{{ $visit->id }}')">
+                                <i class="fa-solid fa-pencil"></i>
+                            </span>
+                            <span class="font-medium text-red-500 cursor-pointer"
+                                  wire:click="$emit('deleteVisit', '{{ $visit->id }}')">
+                                <i class="fa-solid fa-trash"></i>
+                            </span>
+                        </td>
+                    </tr>
+                @endforeach
+            </x-slot>
+        </x-table>
+
+        @if($visit->hasPages())
+            <div class="mt-6">
+                {{ $visit->links() }}
+            </div>
+        @endif
+    @else
+        <p class="mt-4">No se han encontrado resultados</p>
+    @endif
+
+    {{-- Modal show --}}
+    <x-jet-dialog-modal wire:model="detailsModal.open">
+        <x-slot name="title">
+            <span class="text-2xl">Detalles de la Visita #{{ $detailsModal['id'] }}</span>
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="space-y-3">
+                <div>
+                    <x-jet-label>
+                        Hora: {{ $detailsModal['hour']}}
+                    </x-jet-label>
+                </div>
+                <div>
+                    <x-jet-label>
+                        Id Dispositivo: {{ $detailsModal['deviceid'] }}
+                    </x-jet-label>
+                </div>
+                <div>
+                    <x-jet-label>
+                        Version de la App: {{ $detailsModal['appversion'] }}
+                    </x-jet-label>
+                </div>
+                <div>
+                    <x-jet-label>
+                        Agente: {{ $detailsModal['useragnt'] }}
+                    </x-jet-label>
+                </div>
+                <div>
+                    <x-jet-label>
+                        Agente: {{ $detailsModal['useragnt'] }}
+                    </x-jet-label>
+                </div>
+                <div>
+                    <x-jet-label>
+                        Agente: {{ $detailsModal['useragnt'] }}
+                    </x-jet-label>
+                </div>
+                <div>
+                    <x-jet-label>
+                        Punto de Interest: {{ $detailsModal['point_of_interest_id'] }}
+                    </x-jet-label>
+                </div>
+                <div>
+                    <x-jet-label>
+                        Fecha de creación: {{ $detailsModal['createdAt'] }}
+                    </x-jet-label>
+                </div>
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-button wire:click="$set('detailsModal.open', false)">
+                Cerrar
+            </x-button>
+        </x-slot>
+    </x-jet-dialog-modal>
+
+    @push('scripts')
+        <script>
+            Livewire.on('deleteVisit', visitId => {
+                Swal.fire({
+                    title: '¿Quieres eliminar esta Visita?',
+                    text: 'Esta operación es irreversible',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    cancelButtonText: "Cancelar",
+                    confirmButtonColor: '#d33',
+                    cancelButtonColor: '#3085d6',
+                    confirmButtonText: 'Eliminar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        Livewire.emitTo('admin.visit.show-visit', 'delete', visitId)
+                        Swal.fire(
+                            '¡Hecho!',
+                            'La visita ha sido eliminado.',
+                            'success'
+                        )
+                    }
+                })
+            });
+        </script>
+    @endpush
+</div>
