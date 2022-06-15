94383cb671ee4b9709dd972d6cc3b0995ca3887f
David Alegría <david.alg88@gmail.com>
2022-06-15 23:43:46 +0200

    Controlador y vista Welcome

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Place;
+use App\Models\Video;
+
 class WelcomeController extends Controller
 {
     public function __invoke()
     {
-        return view('welcome');
+        $places = Place::all();
+
+        return view('welcome', compact('places'));
     }
 }
