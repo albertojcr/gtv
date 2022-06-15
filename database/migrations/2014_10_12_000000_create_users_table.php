4567e0f2a2cd5df6853ba36e60973aa38799fe85
jmdb <juan.manuel.delgado.barranco@gmail.com>
2022-06-15 19:39:39 +0200

    Arreglo parar evitar el error SQLSTATE[42S22]:

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,8 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('deleted_at')->nullable();
+
 
             $table->string('login', 45);
             $table->string('salt', 45); // TODO generar salt al crear usuarios
