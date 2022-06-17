cfddf07bc548d9411683361d97962ded8b1b5505
Alberto Caparrós Ruiz <albertoj.caparros@pccomponentes.com>
2022-06-17 12:38:40 +0200

    - Corregido acceso al botón de añadir en puntos y lugares si eres estudiante

--- resources/views/livewire/admin/point/show-point.blade.php
+++ resources/views/livewire/admin/point/show-point.blade.php
@@ -2,13 +2,11 @@
     <div class="flex items-center mb-6">
         <h1 class="text-2xl font-semibold text-gray-700">Listado de puntos de interés</h1>
 
-        @hasanyrole('Administrador|Profesor')
         <button type="button"
                 class="ml-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ml-auto"
                 wire:click="$emitTo('admin.point.create-point', 'openCreationModal')">
             Añadir
         </button>
-        @endhasanyrole
     </div>
 
     @livewire('admin.point.create-point')
